1dd5b6dfd86da3ce787c64fa2d4403bf4606cbb2
jtomahogh <46382420+jtomahogh@users.noreply.github.com>
2024-05-22 15:11:13 +0200

    button style

--- frontend/src/components/cart/OrderPopup.tsx
+++ frontend/src/components/cart/OrderPopup.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './OrderPopup.css';
+import './../../core/input/Buttons/ButtonStyle.css';
 
 type PopupProbs = {
     trigger: boolean;
@@ -9,26 +10,28 @@ type PopupProbs = {
 export function OrderPopup(props: PopupProbs) {
     var name, mail, number, agb;
     return (props.trigger?
-        <div className="popup">
-            <div className="wrapper popup-inner">
-                <div> <label>Name: </label> </div>
-                <div> <input type="text" value={name} ></input> </div>
-                <div> <label>Email: </label> </div>
-                <div> <input type="mail" value={mail} ></input> </div>
-                <div> <label>Telefon: </label> </div>
-                <div> <input type="text" value={number} ></input> </div>
-            </div>
+        <div className="overlay">
+            <div className="popup">
+                <div>
+                    <div> <label>Name: </label> </div>
+                    <div> <input type="text" value={name} ></input> </div>
+                    <div> <label>Email: </label> </div>
+                    <div> <input type="mail" value={mail} ></input> </div>
+                    <div> <label>Telefon: </label> </div>
+                    <div> <input type="text" value={number} ></input> </div>
+                </div>
 
-            <div>
-                <input type="checkbox" value={agb} ></input>
-                <label>Hiermit stimme ich den AGB's zu.</label>
-            </div>
+                <div>
+                    <input type="checkbox" value={agb} ></input>
+                    <label>Hiermit stimme ich den AGB's zu.</label>
+                </div>
 
-            <div>
-                <button onClick={() => props.setTrigger(false)}>Abbrechen</button>
-                <button>Abschicken</button>
-            </div>
+                <div>
+                    <button onClick={() => props.setTrigger(false)} className="button">Abbrechen</button>
+                    <button className="button">Abschicken</button>
+                </div>
 
+            </div>
         </div>
         :<div />);
     
