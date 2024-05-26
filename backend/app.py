c17ecdd1c9cf9c2edee19ebcd5bffea5b28b3334
Philipp Richter <p.richter2003@gmail.com>
2024-05-26 11:45:38 +0200

    feat: add Upload View for file upload

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import graphene
 from flask_graphql import GraphQLView
+from graphene_file_upload.flask import FileUploadGraphQLView as UploadView
 from config import app, db
 from schema_queries import Query
 from schema_mutations import Mutations
@@ -8,7 +9,7 @@ schema = graphene.Schema(query=Query, mutation=Mutations)
 
 app.add_url_rule(
     '/graphql',
-    view_func=GraphQLView.as_view(
+    view_func=UploadView.as_view(
         'graphql',
         schema=schema,
         graphiql=True
