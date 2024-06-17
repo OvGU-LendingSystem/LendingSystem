657669103bf71de6b9045a0c1f31998098fd1466
Philipp Richter <p.richter2003@gmail.com>
2024-06-17 11:41:56 +0200

    Refactor models.py to rename delivery_time to return_date in PhysicalObject_Order table; add string representation to Order and physicalObject_Order; add functions to remove physobject from orders

--- backend/models.py
+++ backend/models.py
@@ -86,13 +86,16 @@ class PhysicalObject_Order(Base):
     additionally holds the orderStatus a physicalobject has in an order
     """
     __tablename__       = "physicalobject_order"
-    phys_id             = Column(Integer,       ForeignKey('physicalobject.phys_id'),   primary_key=True)
-    order_id            = Column(Integer,       ForeignKey('order.order_id'),           primary_key=True)
+    phys_id             = Column(Integer,           ForeignKey('physicalobject.phys_id'),   primary_key=True)
+    order_id            = Column(Integer,           ForeignKey('order.order_id'),           primary_key=True)
     order_status        = Column(Enum(orderStatus), nullable = False, default = 'pending')
-    delivery_time       = Column(DateTime,      nullable = True)
+    return_date         = Column(DateTime,          nullable = True)
 
-    physicalobject      = relationship("PhysicalObject", back_populates = "orders")
-    order               = relationship("Order", back_populates = "physicalobjects")
+    physicalobject      = relationship("PhysicalObject",    back_populates = "orders")
+    order               = relationship("Order",             back_populates = "physicalobjects")
+
+    def __repr__(self):
+        return "Physical Object ID: " + str(self.phys_id) + "; Order ID: " + str(self.order_id) + "; Status: " + str(self.order_status)
 
 
 
@@ -152,7 +155,6 @@ class Order(Base):
     """
     __tablename__       = "order"
     order_id            = Column(Integer,           primary_key = True)
-    # status              = Column(Enum(orderStatus), nullable = False, default = 'pending')
     from_date           = Column(DateTime,          unique = False, nullable = False)
     till_date           = Column(DateTime,          unique = False, nullable = False)
 
@@ -166,6 +168,24 @@ class Order(Base):
         tmp = PhysicalObject_Order(order = self, physicalobject = physicalobject, order_status = order_status)
         self.physicalobjects.append(tmp)
         physicalobject.orders.append(tmp)
+
+    def removePhysicalObject(self, physicalobject):
+        """
+        removes a physicalObject from the order
+        """
+        self.physicalobjects.remove(physicalobject)
+        physicalobject.orders.remove(physicalobject)
+
+    def removeAllPhysicalObjects(self):
+        """
+        removes all physicalObjects from the order
+        """
+        for physicalobject in self.physicalobjects:
+            self.physicalobjects.remove(physicalobject)
+            physicalobject.orders.remove(physicalobject)
+
+    def __repr__(self):
+        return "Order ID: " + str(self.order_id) + "; From: " + str(self.from_date) + "; Till: " + str(self.till_date)
 
 class User(Base):
     """
