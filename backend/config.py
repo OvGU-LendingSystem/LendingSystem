5987f08a3e1e6a93fd6f8cc86ba899374ef148ea
Philipp Richter <p.richter2003@gmail.com>
2024-07-10 12:55:36 +0200

    feat: cleanup config file

--- backend/config.py
+++ backend/config.py
@@ -1,74 +1,44 @@
+import configparser
 from flask import Flask
-from sqlalchemy import *
 from flask_cors import CORS
 from flask_session import Session
-from sqlalchemy.orm import (scoped_session, sessionmaker)
-
-import configparser
-import socket
 import os
+import socket
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 
 hostname = socket.gethostname()
 print("Hostname: ", hostname)
 
-# Create connection depending on Host
+config = configparser.ConfigParser()
+# Read config file on host
 if hostname == "hades":
-    # Read database config from file
-    config = configparser.ConfigParser()
-    config.read('/var/www/LendingSystem/config.ini')  # Path to config file
-    db_database = config.get('DB', 'db_LendingSystem_Database')
-    db_user = config.get('DB', 'db_LendingSystem_User')
-    db_pw = config.get('DB', 'db_LendingSystem_Password')
-
-    # Connect to database
-    engine = create_engine('mysql+mysqlconnector://' + db_user + ':' + db_pw + '@localhost/' + db_database)
-    db = scoped_session(sessionmaker(autocommit=False,
-                                     autoflush=False,
-                                     bind=engine))
-
-    bcrypt = Bcrypt(app)
-elif hostname == "container":
-    config = configparser.ConfigParser()
-    config.read('./config.ini') # Path to config file
-    db_pw = config.get('DB', 'db_LendingSystem_password')
-
-    # Create Flask app
-    app = Flask(__name__)
-    app.debug = True
-    CORS(app, resources={r"/*": {"origins": "*"}})
-
-    # Connect to database
-    engine = create_engine('mysql+mysqlconnector://root:' + db_pw + '@db:3306/LendingSystem')
-    db = scoped_session(sessionmaker(   autocommit=False,
-                                        autoflush=False,
-                                        bind=engine))
-
-    bcrypt = Bcrypt(app)
+    config.read("/var/www/LendingSystem/config.ini")
+    db_host = "localhost"
 else:
-    # Read database config from file
-    config = configparser.ConfigParser()
-    config.read('../config.ini')  # Path to config file
-    db_database = config.get('DB', 'db_LendingSystem_Database')
-    db_user = config.get('DB', 'db_LendingSystem_User')
-    db_pw = config.get('DB', 'db_LendingSystem_Password')
+    config.read("../config.ini")
+    db_host = "hades.fritz.box:3306"
 
-    if ((int)(config.get('TESTING', 'testing'))):
-        engine = create_engine('sqlite:///:memory:')
-        db = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-        print("Using Database for Testing")  #TODO REMOVE
-    else:
-        # Connect to database
-        engine = create_engine('mysql+pymysql://' + db_user + ':' + db_pw + '@hades.fritz.box:3306/' + db_database)
-        db = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-        print("Using Production Database")  #TODO REMOVE
+# Read config from File
+db_database             = config.get('DB', 'db_LendingSystem_Database')
+db_user                 = config.get('DB', 'db_LendingSystem_User')
+db_pw                   = config.get('DB', 'db_LendingSystem_Password')
+root_directory          = config.get('PATHS', 'root_directory')
+tmp_picture_directory   = config.get('PATHS', 'picture_directory')
+tmp_pdf_directory       = config.get('PATHS', 'pdf_directory')
+picture_directory       = os.path.join(root_directory, tmp_picture_directory)
+pdf_directory           = os.path.join(root_directory, tmp_pdf_directory)
 
-root_directory = config.get('PATHS', 'root_directory')
-tmp_picture_directory = config.get('PATHS', 'picture_directory')
-tmp_pdf_directory = config.get('PATHS', 'pdf_directory')
-picture_directory = os.path.join(root_directory, tmp_picture_directory)
-pdf_directory = os.path.join(root_directory, tmp_pdf_directory)
+testing_on              = config.get('TESTING', 'testing')
+
+# Create engine depending on test mode
+if not (int)(testing_on):
+    engine = create_engine('mysql+pymysql://' + db_user + ':' + db_pw + '@' + db_host + '/' + db_database)
+else:
+    engine = create_engine('sqlite:///:memory:')
+    
+db = scoped_session(sessionmaker(autocommit = False, autoflush = False, bind = engine))
 
 # Create Flask app
 app = Flask(__name__)
@@ -76,13 +46,11 @@ app.debug = True
 app.secret_key = config.get('SECRET_KEY', 'secret_key')
 app.config['SESSION_TYPE'] = 'sqlalchemy'
 app.config['SESSION_SQLALCHEMY'] = db
-if hostname == "hades":
-    app.config[
-        'SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://' + db_user + ':' + db_pw + '@localhost/' + db_database
-elif ((int)(config.get('TESTING', 'testing'))):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+
+if not (int)(testing_on):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + db_user + ':' + db_pw + '@' + db_host + '/' + db_database
 else:
-    app.config[
-        'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + db_user + ':' + db_pw + '@hades.fritz.box:3306/' + db_database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+
 CORS(app, resources={r"/*": {"origins": "*"}})
 server_session = Session(app)
