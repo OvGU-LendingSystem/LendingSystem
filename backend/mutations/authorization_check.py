6bee79ce5f6e134623f9ee2849c5950b2358f2af
Philipp Richter <p.richter2003@gmail.com>
2024-12-18 11:52:29 +0100

    feat: refactor authorization checks to streamline organization rights validation for physical objects, tags, groups, and orders

--- backend/mutations/authorization_check.py
+++ backend/mutations/authorization_check.py
@@ -11,90 +11,86 @@ def is_authorised(required_rights, executive_user_id, phys_id=None, organization
         if user_org.rights == userRights.system_admin:
             return True
 
-
-    if not executive_user:
-        raise VerificationError("Executive User nicht gefunden")
-
-    # Collect all relevant organizations
-    organization_id = []
-
-    # wenn phys_objects bearbeitet oder gelöscht werden sollen
+    # check rights for given physical object
     if phys_id:
-        tmp = get_organizationID_from_phys_object(phys_id)
-        if not tmp in organization_id:
-            organization_id.append(tmp)
+        return check_for_phys_object(executive_user, required_rights, phys_id)
 
-    # wenn tags bearbeitet oder gelöscht werden sollen
+    # check rights for given tag
     if tag_id:
-        tmp = get_organizationID_from_tag(tag_id)
-        for org_id in tmp:
-            if not org_id in organization_id:
-                organization_id.append(org_id)
+        return check_for_tag(executive_user, required_rights, tag_id)
 
-    # wenn groups bearbeitet oder gelöscht werden sollen
+    # check rights for given group
     if group_id:
-        tmp = get_organization_id_from_group(group_id)
-        if not tmp in organization_id:
-            organization_id.append(tmp)
+        return check_for_group(executive_user, required_rights, group_id)
 
-    if order_id:
-        tmp = get_organization_id_from_order(order_id)
-        for org_id in tmp:
-            if not org_id in organization_id:
-                organization_id.append(org_id)
+    return False
 
-    # Check if user has the required rights in all relevant organizations
-    for org_id in organization_id:
+
+def check_for_phys_object(executive_user, required_rights, phys_id):
+    """ 
+    For editing  physical object the user has to be minimum the required right in the owner organization 
+    """
+    phys_obj = PhysicalObjectModel.query.filter(PhysicalObjectModel.phys_id == phys_id).first()
+    if not phys_obj:
+        raise VerificationError("physikalisches Object nicht gefunden")
+    
+    owner_organization = phys_obj.organization
+    user_relation = owner_organization.users.filter(UserModel.user_id == executive_user.user_id).first()
+
+    return user_relation.user_rights > required_rights
+
+def check_for_tag(executive_user, required_rights, tag_id):
+    """
+    tag can only be edited or deleted by user if he is hat least the required rights in all organizations to which the objects belonging to the tag belong
+    """
+    tag = TagModel.query.filter(TagModel.tag_id == tag_id).first()
+
+    # get all related organizations
+    tag_phys_organizations = []
+    for phys_obj in tag.physicalobjects:
+        if not tag_phys_organizations.contains(phys_obj.organization_id):
+            tag_phys_organizations.append(phys_obj.organization_id)
+
+    # check if user has the required rights in all organizations
+    for org_id in tag_phys_organizations:
         organization = OrganizationModel.query.filter(OrganizationModel.organization_id == org_id).first()
-        user_relation = organization.users.filter(UserModel.user_id == executive_user_id).first()
+        user_relation = organization.users.filter(UserModel.user_id == executive_user.user_id).first()
         
         if not user_relation.user_rights > required_rights:
             return False
         
     return True
 
-
-def get_organizationID_from_phys_object(phys_id):
-    phys_obj = PhysicalObjectModel.query.filter(PhysicalObjectModel.phys_id == phys_id).first()
-    if not phys_obj:
-        raise VerificationError("physikalisches Object nicht gefunden")
-    
-    return phys_obj.organization_id
-
-def get_organizationID_from_tag(tag_id):
-    # ein tag soll nur vom user bearbeitet oder gelöscht werden können,
-    # wenn er in allen Orgas, zu denen die Objekte gehören, die dem Tag angehören, mindestens ein inventory_admin ist
-    tag = TagModel.query.filter(TagModel.tag_id == tag_id).first()
-    if not tag:
-        raise VerificationError("Tag nicht gefunden")
-
-    # alle organization_ids die mit den phys_objs des tags verknüpft sind
-    tag_phys_organizations = []
-    for phys_obj in tag.physicalobjects:
-        if tag_phys_organizations.contains(phys_obj.organization_id):
-            tag_phys_organizations.append(phys_obj.organization_id)
-
-    return tag_phys_organizations
-
-def get_organization_id_from_group(group_id):
+def check_for_group(executive_user, required_rights, group_id):
+    """
+    group can only be edited or deleted by user if he is hat least the required rights in all organizations to which the objects belonging to the group belong
+    """
     group = GroupModel.query.filter(GroupModel.group_id == group_id).first()
-    if not group:
-        raise VerificationError("Group nicht gefunden")
-    organization_id = group.physicalobjects[0].organization_id
-
-    return organization_id
-
-def get_organization_id_from_order(order_id):
-    order = OrderModel.query.filter(OrderModel.order_id == order_id).first()
-    if not order:
-        raise VerificationError("Order nicht gefunden")
     
-    organization_id = []
-    for obj in order.physicalobjects:
-        if not obj.organization_id in organization_id:
-            organization_id.append(obj.organization_id)
+    # get all related organizations
+    group_phys_organizations = []
+    for phys_obj in group.physicalobjects:
+        if not group_phys_organizations.contains(phys_obj.organization_id):
+            group_phys_organizations.append(phys_obj.organization_id)
 
-    return organization_id
+    # check if user has the required rights in all organizations
+    for org_id in group_phys_organizations:
+        organization = OrganizationModel.query.filter(OrganizationModel.organization_id == org_id).first()
+        user_relation = organization.users.filter(UserModel.user_id == executive_user.user_id).first()
+        
+        if not user_relation.user_rights > required_rights:
+            return False
+
+    return True
+
+def check_for_order(executive_user, required_rights, order_id):
+    """
+    order can only be edited or deleted by user if the user is part of  the order users
+    """
+    order = OrderModel.query.filter(OrderModel.order_id == order_id).first()
+    
+    user_relation = order.users.filter(UserModel.user_id == executive_user.user_id).first()
+    return user_relation.user_rights > required_rights
 
 
 
