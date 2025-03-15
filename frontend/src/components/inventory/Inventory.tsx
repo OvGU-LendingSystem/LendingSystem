540fea68240117769fc2a74ebdf936134c8785f5
David Busse <davidbusse343@gmx.de>
2025-03-15 16:13:39 +0100

    change: pdf worker

--- frontend/src/components/inventory/Inventory.tsx
+++ frontend/src/components/inventory/Inventory.tsx
@@ -416,10 +416,7 @@ export function Inventory(): JSX.Element {
                     ref={textRef} 
                     style={{ margin: 0, padding: '10px', maxHeight: '400px', overflowY: 'auto' }}
                 >
-              <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-                    <Viewer fileUrl={'http://192.168.178.169/pdfs/' + selectedManualPath}  plugins={[zoomPluginInstance]}/>
-                </Worker>
-
+                  <Viewer fileUrl={'http://192.168.178.169/pdf/' + selectedManualPath}  plugins={[zoomPluginInstance]}/>
             </div>
             <div>
               <button
